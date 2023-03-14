dd889900b6c8b6e75bb20a9751a5bf914bb9a848
albert.liu435 <albert.liu435@gmail.com>
2023-03-14 22:56:58 +0800

    init

---
+++ rookie-spring-security/rookie-felord-security/src/main/java/com/rookie/bigdata/service/impl/UserServiceImpl.java
@@ -0,0 +1,74 @@
+package com.rookie.bigdata.service.impl;
+
+import com.rookie.bigdata.service.UserService;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author rookie
+ * @Description  参考  UserDetailsService UserDetailsManager InMemoryUserDetailsManager 进行处理
+ * @Date 2023/3/14 22:36
+ * @Version 1.0
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    //InMemoryUserDetailsManager
+
+    private Map<String, UserDetails> users = new HashMap<>();
+
+
+    @Override
+    public void createUser(UserDetails user) {
+        users.putIfAbsent(user.getUsername(), user);
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+        users.put(user.getUsername(), user);
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        users.remove(username);
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+        Authentication currentUser = SecurityContextHolder.getContext()
+                .getAuthentication();
+
+        if (currentUser == null) {
+            // This would indicate bad coding somewhere
+            throw new AccessDeniedException(
+                    "Can't change password as no Authentication object found in context "
+                            + "for current user.");
+        }
+
+        String username = currentUser.getName();
+
+        UserDetails user = users.get(username);
+
+
+        if (user == null) {
+            throw new IllegalStateException("Current user doesn't exist in database.");
+        }
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return users.containsKey(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return users.get(username);
+    }
+}
